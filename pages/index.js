56ad37363ef77acd73400d2a0da314e7f2249651
Ryan Kirsch <agalloch88@gmail.com>
2022-03-27 14:32:27 -0400

    create loadNFTs function, display for nothing in marketplace

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,47 @@ import { ethers } from "ethers";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import Web3Modal from "web3modal";
+
+import { nftaddress, nftmarketaddress } from "../config";
+
+import NFT from '../artifacts/contracts/NFT.sol/NFT.json';
+import Market from '../artifacts/contracts/NFTmarket.sol/NFTMarket.json';
 export default function Home() {
+  const [nfts, setNfts] = useState([]);
+  const [loadingState, setLoadingState] = useState('not-loaded');
+
+  useEffect(() => {
+    loadNFTS()
+  }, [])
+
+  async function loadNFTS() {
+    const provider = new ethers.providers.JsonRpcProvider();
+    const tokenContract = new ethers.Contract(nftaddress, NFT.abi, provider);
+    const marketContract = new ethers.Contract(nftmarketaddress, Market.abi, provider);
+    const data = await marketContract.fetchMarketItems();
+
+    const items = await Promise.all(data.map(async i => {
+      const tokenUri = await tokenContract.tokenURI(i.tokenId);
+      const meta = await axios.get(tokenUri);
+      let price = ethers.utils.formatUnits(i.price.toString(), 'ether');
+      let item = {
+        price,
+        tokenId: i.tokenId.toNumber(),
+        seller: i.seller,
+        owner: i.owner,
+        image: meta.data.image,
+        name: meta.data.name,
+        description: meta.data.description,
+      }
+      return item;
+    }))
+    setNfts(items);
+    setLoadingState('loaded');
+  }
+  if (loadingState === 'loaded' && !nfts.length) return (
+    <h1 className="px-20 py-10 text-3xl">No items in marketplace</h1>
+  )
+
   return (
     <div className="">
       <h1>Home</h1>
