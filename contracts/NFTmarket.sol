d0c0569529107dc1ecf6e4bee160436db88d0610
Ryan Kirsch <agalloch88@gmail.com>
2022-03-27 07:11:07 -0400

    fetchMyNFTs function to display all purchase items

--- contracts/NFTmarket.sol
+++ contracts/NFTmarket.sol
@@ -117,4 +117,28 @@ contract NFTMarket is ReentrancyGuard {
         }
         return items;
     }
+
+//function to return all purchased NFTs of the user for FE use
+    function fetchMyNFTs() public view returns (MarketItem[] memory) {
+        uint totalItemCount = _itemIds.current();
+        uint itemCount = 0;
+        uint currentIndex = 0;
+
+        for (uint i = 0; i < totalItemCount; i++) {
+            if (idToMarketItem[i + 1].owner == msg.sender) {
+                itemCount++;
+            }
+        }
+
+        MarketItem[] memory items = new MarketItem[](itemCount);
+        for (uint i = 0; i < totalItemCount; i++) {
+            if (idToMarketItem[i + 1].owner == msg.sender) {
+                uint currentId = idToMarketItem[i + 1].itemId;
+                MarketItem storage currentItem = idToMarketItem[currentId];
+                items[currentIndex] = currentItem;
+                currentIndex++;
+            }
+        }
+        return items;
+    }
 }
